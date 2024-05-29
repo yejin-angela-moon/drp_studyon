1a971eecad867347255a4af6b2708e7af53559fa
Kang Victor <victork03@naver.com>
2024-05-29 20:23:48 +0100

    feat(StudyOn): establish basic swift app and XCTest framework

---
+++ StudyOn/StudyOn/StudyOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyOnApp.swift
+//  StudyOn
+//
+//  Created by Victor Kang on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct StudyOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
