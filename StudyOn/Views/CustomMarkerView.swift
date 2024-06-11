9b9b3fb2a4aa122d8475ee8c8c9033147543e510
ym1721 <ym1721@ic.ac.uk>
2024-06-11 23:06:33 +0100

    fix: change the map icon colours to be more distinct

--- StudyOn/Views/CustomMarkerView.swift
+++ StudyOn/Views/CustomMarkerView.swift
@@ -11,9 +11,20 @@ struct CustomMarkerView: View {
     
     func colorForRating(_ rating: Double) -> Color {
         let clampedRating = min(max(rating, 0), 5)
-        let green = clampedRating / 5.0
-        let red = (5.0 - clampedRating) / 5.0
-        return Color(red: red, green: green, blue: 0.0)
+        let normalizedRating = clampedRating / 5.0
+        
+        let red: Double
+        let green: Double
+        
+        if normalizedRating <= 0.5 {
+            red = 1.0
+            green = normalizedRating * 2.0
+        } else {
+            red = (1.0 - normalizedRating) * 2.0
+            green = 1.0
+        }
+            
+        return Color(red: red * 0.85, green: green * 0.85, blue: 0.0)
     }
     
     private func imageForCategory(_ category: String) -> String {
